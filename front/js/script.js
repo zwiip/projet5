6b1698dc0fa68c9d59a15fbe48f7a889572229b8
zwiip <astri.brugeilles@gmail.com>
2022-11-05 23:21:31 +0100

    correction et débogage de ma boucle for

--- front/js/script.js
+++ front/js/script.js
@@ -1,9 +1,8 @@
 /* --- Javascript lié à index.html permettant d'afficher dynamiquement les liens vers les pages produits ---*/
 
 /* Récupération des données de l'API, puis création des élements dans la page d'accueil grâce à une boucle for. */
-let getData = async () => {
-    await fetch("http://localhost:3000/api/products")
-    .then(function(res) {
+const getData = fetch("http://localhost:3000/api/products");
+    getData.then(function(res) {
         if (res.ok) {
             return res.json();
         }
@@ -12,36 +11,32 @@ let getData = async () => {
         console.log(links);
         for (let i = 0; i < links.length; i++) {
             
-            let linkHome = document.createElement('a');
+            const linkHome = document.createElement('a');
             document.querySelector('#items').append(linkHome);
-            homeLink.href = `#`;
+            linkHome.href = `./product.html?id=${links[i]._id}`;
             
-            let linkArticle = document.createElement('article');
-            document.querySelector(linkHome).append(linkArticle);
+            const linkArticle = document.createElement('article');
+            linkHome.append(linkArticle);
             
-            let linkImg = document.createElement('img');
+            const linkImg = document.createElement('img');
             linkImg.src = links[i].imageUrl;
             linkImg.alt = links[i].altTxt;
-            document.querySelector(linkArticle).append(linkImg);
+            linkArticle.append(linkImg);
 
-            let linkName = document.createElement('h3');
+            const linkName = document.createElement('h3');
             linkName.classList.add('productName');
             linkName.innerText = links[i].name;
-            document.querySelector(linkArticle).append(linkName);
+            linkArticle.append(linkName);
             
-            let linkDescription = document.createElement('p');
-            linkName.classList.add('productDescription');
-            linkName.innerText = links[i].description;
-            document.querySelector(linkArticle).append(linkDescription);
+            const linkDescription = document.createElement('p');
+            linkDescription.classList.add('productDescription');
+            linkDescription.innerText = links[i].description;
+            linkArticle.append(linkDescription);
         }
     })
     .catch(function(err) {
         console.log("une erreur est survenue")
     });
-}
-
-getData();
-
 
 
 /* create element
