a1789805032192336fda2dd0cf5c118f85a8480a
zwiip <astri.brugeilles@gmail.com>
2022-11-06 22:41:40 +0100

    récupération et positionnement des infos produits dans la page

--- front/js/product.js
+++ front/js/product.js
@@ -13,3 +13,37 @@ const getID = new URLSearchParams(searchURL);
 console.log(getID);
 
 const productID = getID.get('id')
+
+/** 
+ * Permet d'afficher le bon produit selon son ID en modifiant l'id dans l'appel de l'API
+ */
+let pageProduct = fetch(`http://localhost:3000/api/products/${productID}`);
+    pageProduct.then(function(res) {
+        if (res.ok) {
+            return res.json();
+        }
+    })
+    .then(function(productData) {
+        console.log(productData);
+        
+        const productImg = document.createElement('img');
+        productImg.src = productData.imageUrl;
+        productImg.alt = productData.altTxt;
+        document.querySelector('div.item__img').append(productImg);
+
+        const productName = productData.name;
+        document.querySelector('#title').textContent = `${productName}`;
+
+        const productPrice = productData.price;
+        document.querySelector('#price').textContent = `${productPrice}`;
+
+        const productDescription = productData.description;
+        document.querySelector('#description').textContent = `${productDescription}`;
+
+    })
+/**
+ * Permet de signaler si une erreur est apparue dans les fonctions précédentes
+ */
+    .catch(function(err) {
+        console.log("impossible de charger la page produit")
+    });
