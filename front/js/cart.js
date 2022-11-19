cefdee5f5515f0e782ebfcb7bcfddb0fc27c4c69
zwiip <astri.brugeilles@gmail.com>
2022-11-19 08:05:50 +0100

    ajout fonction main

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,12 +1,32 @@
-// récupération des éléments stockés dans le local storage
+function main() {
+    getListProducts();
+    getProductData(listProducts);
+    if(getListProducts && getProductData) {
+        console.log('toutes les données sont récupérées');
+        checkCartContent(listProducts);
+    } else {
+        console.log('impossible de charger les données');
+        return
+    }
 
-function getListProducts() {
-    let listProducts = JSON.parse(localStorage.getItem("listProducts"));
-    console.log(listProducts);
-    checkCartContent(listProducts);
+// récupération des éléments stockés dans le local storage
+    function getListProducts() {
+        let listProducts = JSON.parse(localStorage.getItem("listProducts"));
+        console.log(listProducts);
+        return listProducts
+    }
+    
+// récupération des données de l'API
+    function getProductData(listProducts) {
+        let productData = fetch(`http://localhost:3000/api/products`);
+        productData.then(function(res) {
+            if (res.ok) {
+                return res.json();
+            }
+        })
+    }
 
     // vérification du contenus du panier
-
     function checkCartContent(listProducts) {
         const cartItems = document.querySelector("#cart__items");
         if(listProducts == null || listProducts.length === 0 ) {
@@ -20,37 +40,34 @@ function getListProducts() {
         }
     }
 
+};
+
+
     function displayCart(listProducts) {
         console.log("création des éléments pour le panier");
         for (i = 0; i < listProducts.length; i ++) {
             
+            
+// création du bloc article
             const article = document.createElement('article');
             document.querySelector("#cart__items").append(article);
             article.classList.add('cart__item');
             article.dataset.id = 'listProducts[i].id'
             article.dataset.color = 'listProducts[i].color'
 
+// création du bloc img
             const cartItemImg = document.createElement('div');
             article.append(cartItemImg);
             article.classList.add('cart__item__img');
-
-            getProductData(listProducts);
             
+// création de l'img
             const img = document.createElement('img');
-            img.src = 'productData.imageUrl;'
-
+            img.src = 'productData[].imageUrl;'
+            cartItemImg.append(img);
         }
+
+
     }
 
-    function getProductData(listProducts) {
-        let productID = 'listProducts[i].id';
-        let productData = fetch(`http://localhost:3000/api/products/${productID}`);
-        productData.then(function(res) {
-            if (res.ok) {
-                return res.json();
-            }
-        })
-    };
-};
 
-getListProducts();
+
